dc133c864e2171c182e28260653dca3e5e74e8cf
hbaltz <hugo.baltz@ensg.eu>
2017-02-24 15:23:00 +0100

    Vérification checksum et afficahge détaillé en argv

---
+++ TP2-parser/src/README.md
@@ -0,0 +1,16 @@
+# Commandes
+
+## Commande de compilation
+```
+gcc -std=c99 -o parser_one parser_one.c
+```
+
+## Commande lancement
+Sans l'affichage détaillée
+```
+./parser_one
+```
+Avec l'affichage détaillée
+```
+./parser_one 1
+```
--- TP2-parser/src/parser_one.c
+++ TP2-parser/src/parser_one.c
@@ -4,8 +4,9 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
-// Strucutre du payload correspondant à un mesaage ubdx de class 01 et d'ID 21
+// Strucutre du payload correspondant à un message ubdx de class 01 et d'ID 21
 struct timeUTC{
 	unsigned int iTown;
 	unsigned int tAcc;
@@ -43,35 +44,54 @@ struct message{
 // Donnée en entrée
 unsigned char data[] = {0xb5, 0x62, 0x01, 0x21, 0x14, 0x00, 0x60, 0x12, 0xe7, 0x1e, 0x0d, 0x00, 0x00, 0x00, 0x6c, 0x77, 0x00, 0x00, 0xe0, 0x07, 0x07, 0x17, 0x00, 0x00, 0x2b, 0x07, 0xd4, 0x01};
 
-// Bool affichage, si true affiche données récupérées
-bool affichage = false; 
-
 // Déclaration fonction
 void afficherDonnee(struct message msg, struct timeUTC tUtc);
 
 int main(int argc, char **argv){
 
+	int argAff;
+
+	if (argv[1] != NULL){
+		argAff = strtod(argv[1],NULL);
+	}
+
 	// Désérialisation
 	struct message msg; 
 	msg = *(struct message *) data;
 
-	// Lecture payload
-	struct timeUTC tUtc;
-	tUtc = * (struct timeUTC *) msg.payload;
-	
 	// Taille data
 	int sizeData = sizeof(data);
 
 	// Récupération Checksum
-	msg.checksum1 = data[sizeData - 1];
-	msg.checksum2 = data[sizeData - 2];
+	msg.checksum1 = data[sizeData-2];
+	msg.checksum2 = data[sizeData-1];
 
-	if(affichage){
-		afficherDonnee(msg,tUtc);
+	unsigned char chks1 = 0, chks2 = 0; // Checksum calculée
+
+	for (int i = 2; i < sizeData-2; ++i){
+		chks1 += data[i];
+		chks2 += chks1;
 	}
 
-	printf("Date d'acquisiton : %u/%u/%u à %u:%u:%u \n",tUtc.day,tUtc.month,tUtc.year,tUtc.hour,tUtc.min,tUtc.sec);
+	// Nous ne continouns les traitements que si les checksum calculés sont égaux aux checksum contenus dans le messaga
+	if(chks1 == msg.checksum1 && chks2==msg.checksum2){
+		// Si le message ubdx est de class 01 et a pour ID 21, on lit la date
+		if(msg.class == 0x01  && msg.ID == 0x21){
+				// Lecture payload
+				struct timeUTC tUtc;
+				tUtc = * (struct timeUTC *) msg.payload;
 
+				
+				// Affichage des données détaillée
+				if(argAff == 1){
+					afficherDonnee(msg,tUtc);
+				}
+
+				// Affichage de la date d'acquisiton
+				printf("Date d'acquisiton : %u/%u/%u à %u:%u:%u \n",
+					tUtc.day,tUtc.month,tUtc.year,tUtc.hour,tUtc.min,tUtc.sec);
+			}
+	}
 	return 0;
 }
 
@@ -96,6 +116,6 @@ void afficherDonnee(struct message msg, struct timeUTC tUtc){
 		printf("valid: %u \n",tUtc.valid);
 
 		// Affichage checkSum
-		printf("Checksum1: %X \n", msg.checksum1);
-		printf("Checksum2: %X \n", msg.checksum1);
+		printf("Checksum1: %u \n", msg.checksum1);
+		printf("Checksum2: %u \n", msg.checksum2);
 }
